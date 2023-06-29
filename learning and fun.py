b0e0ffba957a3a2f3c1ef4c41e3bf44b6e5a57e2
Mandeep Toor <97710257+mandeeptoor10@users.noreply.github.com>
2023-06-29 18:07:13 -0400

    Add files via upload

---
+++ learning and fun.py
@@ -0,0 +1,637 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# Get index of 'germany': ind_ger
+ind_ger=countries.index("germany")
+
+# Use ind_ger to print out capital of Germany
+print(capitals[ind_ger])
+
+
+# In[3]:
+
+
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# From string in countries and capitals, create dictionary europe
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin' ,'norway':'oslo'  }
+
+# Print europe
+print(europe)
+
+
+# In[4]:
+
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Print out the keys in europe
+print(europe.keys())
+
+# Print out value that belongs to key 'norway'
+print(europe['norway'])
+
+
+# In[5]:
+
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Add italy to europe
+europe['italy'] = 'rome'
+# Print out italy in europe
+print('italy' in europe)
+# Add poland to europe
+europe['poland'] = 'warsaw'
+
+# Print europe
+print(europe)
+
+
+# In[6]:
+
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw',
+          'australia':'vienna' }
+
+# Update capital of germany
+europe['germany'] = 'berlin'
+
+# Remove australia
+del(europe['australia'])
+
+# Print europe
+print(europe)
+
+
+# In[7]:
+
+
+# Dictionary of dictionaries
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+
+
+# Print out the capital of France
+print(europe['france']['capital'])
+
+# Create sub-dictionary data
+data = { 'capital':'rome', 'population':59.83 }
+
+# Add data to europe under key 'italy'
+europe['italy'] = data
+
+# Print europe
+print(europe)
+
+
+# In[9]:
+
+
+# Pre-defined lists
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+
+# Import pandas as pd
+import pandas as pd
+
+# Create dictionary my_dict with three key:value pairs: my_dict
+my_dict = { 'country':names, 'drives_right':dr, 'cars_per_cap':cpc}
+
+# Build a DataFrame cars from my_dict: cars
+cars=pd.DataFrame(my_dict)
+
+# Print cars
+print(cars)
+
+
+# In[10]:
+
+
+import pandas as pd
+
+# Build cars DataFrame
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+cars_dict = { 'country':names, 'drives_right':dr, 'cars_per_cap':cpc }
+cars = pd.DataFrame(cars_dict)
+print(cars)
+
+# Definition of row_labels
+row_labels = ['US', 'AUS', 'JPN', 'IN', 'RU', 'MOR', 'EG']
+
+# Specify row labels of cars
+cars.index=(row_labels)
+
+# Print cars again
+print(cars)
+
+
+# In[12]:
+
+
+# Comparison of booleans
+print(True == False)   # False
+print(True != False)   # True
+
+# Comparison of integers
+print(42 > 13)   # True
+print(8 <= 8)    # True
+
+# Comparison of strings
+print('apple' < 'banana')   # True
+print('cat' >= 'dog')       # False
+
+# Compare a boolean with an integer
+print(True == 1)   # True
+print(False == 0)  # True
+print(True > 0)    # True
+print(False < 1)   # True
+print(-5 * 15 != 75)  # True
+print("pyscript" == "PyScript" )
+
+
+# In[13]:
+
+
+# Comparison of integers
+x = -3 * 6
+print(x>=-10)
+print(x!=0)
+
+# Comparison of strings
+y = "test"
+print("test" <= y)
+print(y<="test")
+
+# Comparison of booleans
+print(True >False)   # False
+print(True != False)   # True
+
+
+# In[14]:
+
+
+# Create arrays
+import numpy as np
+my_house = np.array([18.0, 20.0, 10.75, 9.50])
+your_house = np.array([14.0, 24.0, 14.25, 9.0])
+
+# my_house greater than or equal to 18
+print(my_house>=18)
+
+# my_house less than your_house
+print(my_house < your_house)
+
+
+# In[15]:
+
+
+# Define variables
+my_kitchen = 18.0
+your_kitchen = 14.0
+
+# my_kitchen bigger than 10 and smaller than 18?
+print(my_kitchen>10 and my_kitchen <18)
+
+# my_kitchen smaller than 14 or bigger than 17?
+print(my_kitchen<14 or my_kitchen<17)
+
+# Double my_kitchen smaller than triple your_kitchen?
+print(my_kitchen * 2 < your_kitchen * 3)
+
+
+# In[16]:
+
+
+# Create arrays
+import numpy as np
+my_house = np.array([18.0, 20.0, 10.75, 9.50])
+your_house = np.array([14.0, 24.0, 14.25, 9.0])
+
+# my_house greater than 18.5 or smaller than 10
+print(np.logical_or(my_house > 18.5, my_house < 10))   # [False False  True  True]
+
+# Both my_house and your_house smaller than 11
+print(np.logical_and(my_house < 11, your_house < 11))   # [False False False  True]
+
+
+# In[ ]:
+
+
+if else 
+
+
+# In[17]:
+
+
+# Define variables
+room = "kit"
+area = 14.0
+
+# if statement for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+
+# if statement for area
+if area > 15:
+    print('big place!')
+
+
+# In[18]:
+
+
+# Define variables
+room = "kit"
+area = 14.0
+
+# if statement for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+
+# if statement for area
+if area > 15:
+    print('big place!')
+
+
+# In[19]:
+
+
+# Define variables
+room = "bed"
+area = 14.0
+
+# if-elif-else construct for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+elif room == "bed":
+    print("looking around in the bedroom.")
+else :
+    print("looking around elsewhere.")
+
+# if-elif-else construct for area
+if area > 15 :
+    print("big place!")
+elif area > 10:
+    print("medium size, nice!")    
+else :
+    print("pretty small.")
+
+
+# In[ ]:
+
+
+loops
+
+
+# In[22]:
+
+
+# Initialize offset
+offset = 8
+
+# Code the while loop
+while offset!=0:
+    print("correcting...")
+    offset = offset - 1
+    print(offset)
+
+
+# In[23]:
+
+
+# Initialize offset
+offset = -6
+
+# Code the while loop
+while offset != 0 :
+    print("correcting...")
+    if offset > 0 :
+        offset = offset - 1
+    else :
+        offset = offset + 1
+    print(offset)
+
+
+# In[24]:
+
+
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Change for loop to use enumerate() and update print()
+for index, area in enumerate(areas) :
+    print("room " + str(index) + ": " + str(area))
+
+
+# In[25]:
+
+
+# house list of lists
+house = [["hallway", 11.25], 
+         ["kitchen", 18.0], 
+         ["living room", 20.0], 
+         ["bedroom", 10.75], 
+         ["bathroom", 9.50]]
+         
+# Build a for loop from scratch
+for x in house:
+    print("the " + x[0] + " is " + str(x[1]) + " sqm")
+
+
+# In[26]:
+
+
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Adapt the printout
+for index, area in enumerate(areas) :
+    print("room " + str(index + 1) + ": " + str(area))
+
+
+# In[27]:
+
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw', 'austria':'vienna' }
+          
+# Iterate over europe
+for key, value in europe.items() :
+     print("the capital of " + str(key) + " is " + str(value))
+
+
+# In[ ]:
+
+
+random
+
+
+# In[29]:
+
+
+# Import numpy as np
+import numpy as np
+import random
+# Set the seed
+np.random.seed(123)
+
+# Generate and print random float
+print(np.random.rand())
+
+
+# In[30]:
+
+
+# Import numpy and set seed
+import numpy as np
+np.random.seed(123)
+
+# Use randint() to simulate a dice
+print(np.random.randint(1, 7))
+
+# Use randint() again
+print(np.random.randint(1, 7))
+
+
+# In[31]:
+
+
+# NumPy is imported, seed is set
+import numpy as np
+np.random.seed(123)
+
+# Starting step
+step = 50
+
+# Roll the dice
+dice = np.random.randint(1, 7)
+
+# Finish the control construct
+if dice <= 2:
+    step = step - 1
+elif dice <= 5:
+    step = step + 1
+else:
+    step = step + np.random.randint(1, 7)
+
+# Print out dice and step
+print("Dice: ", dice)
+print("Step: ", step)
+
+
+# In[32]:
+
+
+# NumPy is imported, seed is set
+
+# Initialize random_walk
+random_walk = [0]
+
+# Complete the ___
+for x in range(100) :
+    # Set step: last element in random_walk
+    step= random_walk[-1]
+
+    # Roll the dice
+    dice = np.random.randint(1,7)
+
+    # Determine next step
+    if dice <= 2:
+        step = step - 1
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    # append next_step to random_walk
+    random_walk.append(step)
+
+# Print random_walk
+print(random_walk)
+
+
+# In[33]:
+
+
+# NumPy is imported, seed is set
+
+# Initialize random_walk
+random_walk = [0]
+
+for x in range(100) :
+    step = random_walk[-1]
+    dice = np.random.randint(1,7)
+
+    if dice <= 2:
+        # Replace below: use max to make sure step can't go below 0
+        step = max(0, step - 1)
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    random_walk.append(step)
+
+print(random_walk)
+
+
+# In[35]:
+
+
+# NumPy is imported, seed is set
+
+# Initialization
+random_walk = [0]
+
+for x in range(100) :
+    step = random_walk[-1]
+    dice = np.random.randint(1,7)
+
+    if dice <= 2:
+        step = max(0, step - 1)
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    random_walk.append(step)
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Plot random_walk
+plt.plot(random_walk)
+
+# Show the plot
+plt.show()
+
+
+# In[36]:
+
+
+# NumPy is imported; seed is set
+
+# Initialize all_walks (don't change this line)
+all_walks = []
+
+# Simulate random walk 10 times
+for i in range(10) :
+
+    # Code from before
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+        random_walk.append(step)
+
+    # Append random_walk to all_walks
+    all_walks.append(random_walk)
+
+# Print all_walks
+print(all_walks)
+
+
+# In[37]:
+
+
+# numpy and matplotlib imported, seed set.
+
+# initialize and populate all_walks
+all_walks = []
+for i in range(10) :
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Convert all_walks to NumPy array: np_aw
+np_aw= np.array(all_walks)
+
+# Plot np_aw and show
+plt.plot(np_aw)
+plt.show()
+
+# Clear the figure
+plt.clf()
+
+# Transpose np_aw: np_aw_t
+np_aw_t = np.transpose(np_aw)
+
+# Plot np_aw_t and show
+plt.plot(np_aw_t)
+plt.show()
+
+
+# In[39]:
+
+
+# numpy and matplotlib imported, seed set
+
+# Simulate random walk 500 times
+all_walks = []
+for i in range(500) :
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+        if np.random.rand() <= 0.001 :
+            step = 0
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Create and plot np_aw_t
+np_aw_t = np.transpose(np.array(all_walks))
+
+# Select last row from np_aw_t: ends
+ends = np_aw_t[-1,:]
+
+# Plot histogram of ends, display plot
+plt.hist(ends)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
